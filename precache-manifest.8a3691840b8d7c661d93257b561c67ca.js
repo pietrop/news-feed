a2e2d60bbe1d2411b51fd94c75111254d398a542
Pietro Passarelli <pietro.passarelli@wsj.com>
2019-09-18 00:55:17 +0100

    Updates

---
+++ precache-manifest.8a3691840b8d7c661d93257b561c67ca.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "75e1f6a48eeffe027cf6d93ec8370241",
+    "url": "/news-feed/index.html"
+  },
+  {
+    "revision": "ccea53fb1559467be96a",
+    "url": "/news-feed/static/css/2.22a7d4ef.chunk.css"
+  },
+  {
+    "revision": "5669d0983d3daed34167",
+    "url": "/news-feed/static/css/main.33597a57.chunk.css"
+  },
+  {
+    "revision": "ccea53fb1559467be96a",
+    "url": "/news-feed/static/js/2.a1be55f0.chunk.js"
+  },
+  {
+    "revision": "5669d0983d3daed34167",
+    "url": "/news-feed/static/js/main.847b13a1.chunk.js"
+  },
+  {
+    "revision": "06c8c9ea86b11385b8f7",
+    "url": "/news-feed/static/js/runtime~main.05f6cc37.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.ce8212bc456f95f145a44a13d808ddd7.js"
+  "/news-feed/precache-manifest.8a3691840b8d7c661d93257b561c67ca.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/news-feed/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
