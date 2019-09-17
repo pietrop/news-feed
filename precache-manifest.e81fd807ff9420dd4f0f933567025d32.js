d0ae33793e90466e00eb024a6bd67d48c3d80c2f
Pietro Passarelli <pietro.passarelli@wsj.com>
2019-09-17 23:08:06 +0100

    Updates

---
+++ precache-manifest.e81fd807ff9420dd4f0f933567025d32.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "562f114837cd7b7c9d1f0c2854aa5675",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3c4780e695529f86c583",
+    "url": "/static/css/2.22a7d4ef.chunk.css"
+  },
+  {
+    "revision": "ce2b8e601c94db03adf7",
+    "url": "/static/css/main.33597a57.chunk.css"
+  },
+  {
+    "revision": "3c4780e695529f86c583",
+    "url": "/static/js/2.b51fade4.chunk.js"
+  },
+  {
+    "revision": "ce2b8e601c94db03adf7",
+    "url": "/static/js/main.39a1242b.chunk.js"
+  },
+  {
+    "revision": "31cc4b243f00bf3731d3",
+    "url": "/static/js/runtime~main.84b96afe.js"
+  }
+]);
