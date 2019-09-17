144e9d9ee147aa7451555bdc53f1d8ab650a1415
Pietro Passarelli <pietro.passarelli@wsj.com>
2019-09-17 21:26:59 +0100

    Saving

--- README.md
+++ README.md
@@ -1,68 +1,46 @@
-This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
 
-## Available Scripts
+## News Feeds
+<!-- _One liner + link to confluence page_
+_Screenshot of UI - optional_ -->
+A simple searchable list of News Feeds.
 
-In the project directory, you can run:
+## Setup
+<!-- _stack - optional_
+_How to build and run the code/app_ -->
 
-### `npm start`
+git clone, cd, npm install.
 
-Runs the app in the development mode.<br>
-Open [http://localhost:3000](http://localhost:3000) to view it in the browser.
+## Usage
 
-The page will reload if you make edits.<br>
-You will also see any lint errors in the console.
+```
+npm start
+```
 
-### `npm test`
+## System Architecture
+<!-- _High level overview of system architecture_ -->
+Uses [Create React app](https://create-react-app.dev/)
 
-Launches the test runner in the interactive watch mode.<br>
-See the section about [running tests](https://facebook.github.io/create-react-app/docs/running-tests) for more information.
+## Development env
+ <!-- _How to run the development environment_
+_Coding style convention ref optional, eg which linter to use_
+_Linting, github pre-push hook - optional_ -->
+- npm > 6
+- node > 10
 
-### `npm run build`
+## Build
+<!-- _How to run build_ -->
+```
+npm run build
+```
 
-Builds the app for production to the `build` folder.<br>
-It correctly bundles React in production mode and optimizes the build for the best performance.
+## Tests
+<!-- _How to carry out tests_ -->
+```
+npm run test
+```
 
-The build is minified and the filenames include the hashes.<br>
-Your app is ready to be deployed!
+_NA_
 
-See the section about [deployment](https://facebook.github.io/create-react-app/docs/deployment) for more information.
-
-### `npm run eject`
-
-**Note: this is a one-way operation. Once you `eject`, you can’t go back!**
-
-If you aren’t satisfied with the build tool and configuration choices, you can `eject` at any time. This command will remove the single build dependency from your project.
-
-Instead, it will copy all the configuration files and the transitive dependencies (Webpack, Babel, ESLint, etc) right into your project so you have full control over them. All of the commands except `eject` will still work, but they will point to the copied scripts so you can tweak them. At this point you’re on your own.
-
-You don’t have to ever use `eject`. The curated feature set is suitable for small and middle deployments, and you shouldn’t feel obligated to use this feature. However we understand that this tool wouldn’t be useful if you couldn’t customize it when you are ready for it.
-
-## Learn More
-
-You can learn more in the [Create React App documentation](https://facebook.github.io/create-react-app/docs/getting-started).
-
-To learn React, check out the [React documentation](https://reactjs.org/).
-
-### Code Splitting
-
-This section has moved here: https://facebook.github.io/create-react-app/docs/code-splitting
-
-### Analyzing the Bundle Size
-
-This section has moved here: https://facebook.github.io/create-react-app/docs/analyzing-the-bundle-size
-
-### Making a Progressive Web App
-
-This section has moved here: https://facebook.github.io/create-react-app/docs/making-a-progressive-web-app
-
-### Advanced Configuration
-
-This section has moved here: https://facebook.github.io/create-react-app/docs/advanced-configuration
-
-### Deployment
-
-This section has moved here: https://facebook.github.io/create-react-app/docs/deployment
-
-### `npm run build` fails to minify
-
-This section has moved here: https://facebook.github.io/create-react-app/docs/troubleshooting#npm-run-build-fails-to-minify
+## Deployment
+<!-- _How to deploy the code/app into test/staging/production_ -->
+_TBC_
--- src/App.js
+++ src/App.js
@@ -1,26 +1,222 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import 'bootstrap-css-only/css/bootstrap.css';
+import Container from 'react-bootstrap/Container';
+import ListGroup from 'react-bootstrap/ListGroup';
+import Card from 'react-bootstrap/Card';
+import Badge from 'react-bootstrap/Badge';
+import InputGroup from 'react-bootstrap/InputGroup';
+import FormControl from 'react-bootstrap/FormControl';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import OverlayTrigger from 'react-bootstrap/OverlayTrigger';
+import Popover from 'react-bootstrap/Popover';
+import Button from 'react-bootstrap/Button';
+import Select from 'react-select';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import {
+  faSearch,
+} from '@fortawesome/free-solid-svg-icons';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+let Parser = require('rss-parser');
+// longer list of feeds
+// https://blog.feedspot.com/uk_news_rss_feeds/
+const VOX_RSS = 'https://vox.com/rss/index.xml';
+const BBC_RSS = 'http://feeds.bbci.co.uk/news/rss.xml';
+const BBC_TECHNOLOGY_RSS = 'http://feeds.bbci.co.uk/news/video_and_audio/technology/rss.xml'
+const BBC_SCIENCE_AND_ENVIROMENT_RSS = 'http://feeds.bbci.co.uk/news/video_and_audio/science_and_environment/rss.xml';
+const GUARDIAN_WORLD_RSS ='https://www.theguardian.com/world/rss';
+const GUARDIAN_TOP_RSS = 'https://www.theguardian.com/uk/rss';
+const WSJ_WORLD_RSS= 'https://feeds.a.dj.com/rss/RSSWorldNews.xml';
+const WSJ_TECHNOLOGY_RSS = 'https://feeds.a.dj.com/rss/RSSWSJD.xml';
+const NEWS_UK_RSS = 'https://www.news.co.uk/feed';
+const BUZZFEED_US_NEWS_RSS = 'https://www.buzzfeed.com/usnews.xml';
+const QUARTZ_RSS = 'https://qz.com/re/rss';
+
+const RSS_FEEDS = [
+  { 
+    value: VOX_RSS, 
+    label: 'Vox'
+  }, 
+  { 
+    value: BBC_RSS, 
+    label: 'BBC'
+  },
+  { 
+    value: BBC_TECHNOLOGY_RSS, 
+    label: 'BBC - Technology'
+  },
+  { 
+    value: BBC_SCIENCE_AND_ENVIROMENT_RSS, 
+    label: 'BBC - Science And Enviroment'
+  },
+  { 
+    value: GUARDIAN_WORLD_RSS, 
+    label: 'Guardian - World'
+  },
+  { 
+    value: GUARDIAN_TOP_RSS, 
+    label: 'Guardian - Top'
+  },
+  {
+    value: WSJ_WORLD_RSS,
+    label: 'WSJ - World'
+  },
+  {
+    value: WSJ_TECHNOLOGY_RSS,
+    label: 'WSJ - Technology'
+  },
+  {
+    value: NEWS_UK_RSS,
+    label: 'News - UK'
+  },
+  {
+    value: BUZZFEED_US_NEWS_RSS,
+    label: 'Buzz Feed - US News'
+  },
+  {
+    value: QUARTZ_RSS,
+    label: 'Quartz'
+  }
+]
+
+let parser = new Parser();
+const CORS_PROXY = "https://cors-anywhere.herokuapp.com/";
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { 
+      feedItems: [],
+      options: RSS_FEEDS
+     };
+  }
+
+  async componentDidMount(){
+    RSS_FEEDS.forEach( async (rssFeed)=>{
+      let feed = await parser.parseURL(CORS_PROXY + rssFeed.value);
+      const { feedItems } = this.state;
+      const newFeedItems = feed.items.map((item)=>{ item.display = true; item.brand = feed.title; return item;});
+      const updatedFeedItems = feedItems.concat(newFeedItems);
+      this.setState({
+        feedItems: updatedFeedItems, 
+        feedTitle: feed.title
+      })
+    })
+  }
+
+  handleSearch = (e)=>{
+    this.searchRssFeed(e.target.value);
+  }
+
+  searchRssFeed = (searchValue)=>{
+    const rssFeedResults = this.state.feedItems.map((item) => {
+      if (item.title.toLowerCase().includes(searchValue.toLowerCase())
+      || item.contentSnippet.toLowerCase().includes(searchValue.toLowerCase())) {
+        item.display = true;
+
+        return item;
+      } else {
+        item.display = false;
+
+        return item;
+      }
+    });
+
+    this.setState({
+      images: rssFeedResults
+    });
+  }
+
+  // filterRssFeed = (brandValue)=>{
+  //   const rssFeedResults = this.state.feedItems.map((item) => {
+  //     if (item.brand.toLowerCase().includes(brandValue.toLowerCase())) {
+  //       item.display = true;
+
+  //       return item;
+  //     } else {
+  //       item.display = false;
+
+  //       return item;
+  //     }
+  //   });
+
+  //   this.setState({
+  //     images: rssFeedResults
+  //   });
+  // }
+
+  // handleSelectChange = selectedOption => {
+  //   console.log(`Option selected:`, selectedOption)
+  //   if(selectedOption){
+  //     this.setState({ selectedOption });
+  //     this.filterRssFeed(selectedOption[0].label);
+  //   }
+  // };
+
+  render() {
+    const feed = this.state.feedItems.map((item,index)=>{
+      if (item.display) {
+      return (
+        <ListGroup.Item key={item.link+index}>
+          <Row>
+            <Col xl={9} lg={9} md={9} sm={9}  xs={12}>
+              <details>
+                <summary title={item.pubDate}>
+                  <a href= {item.link} target="_blank" rel="noopener noreferrer">{item.title}</a>
+                </summary>
+                <div dangerouslySetInnerHTML={{__html: item.content}}/>
+                <small className={'text-secondary'}>{item.author}</small>
+              </details>
+            </Col>
+            <Col xl={3} lg={3} md={3} sm={3}  xs={12}>
+              <Badge className={'noselect'} variant="dark">{item.brand}</Badge>
+            </Col>
+          </Row>
+        </ListGroup.Item>
+       )
+      }
+      else {
+        return null;
+      }
+    })
+    return (
+      <Container>
+        <Card>
+        {/* <Card.Header>{this.state.feedTitle}</Card.Header> */}
+        <Card.Header>
+          <InputGroup className="mb-3">
+            <InputGroup.Prepend>
+              <InputGroup.Text id="basic-addon1">
+                <FontAwesomeIcon icon={ faSearch } />
+              </InputGroup.Text>
+            </InputGroup.Prepend>
+            <FormControl
+              placeholder="Search news feeds.."
+              aria-label="search"
+              aria-describedby="basic-addon1"
+              onChange={this.handleSearch}
+            />
+          </InputGroup>
+        </Card.Header>
+        {/* <Card.Header>
+            <Select
+            isClearable
+            isMulti
+            value={this.state.selectedOption? this.state.selectedOption : '' }
+            onChange={this.handleSelectChange}
+            options={this.state.options}
+          />
+        </Card.Header> */}
+        <ListGroup variant="flush" style={{ height: '90vh',overflow: 'auto'}}>
+        {/* <ListGroup> */}
+        {feed}
+        {/* </ListGroup> */}
+        </ListGroup>
+        </Card>
+      </Container>
+      ); 
+  }
 }
 
 export default App;
