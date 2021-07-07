730f7e50295c9d068b32096d19e27c545db1c3be
Pietro Passarelli <pietro.passarelli@wsj.com>
2021-07-07 12:26:34 -0400

    various improvements

--- src/App.js
+++ src/App.js
@@ -10,43 +10,118 @@ import FormControl from 'react-bootstrap/FormControl';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import {
-  faSearch,
-} from '@fortawesome/free-solid-svg-icons';
+import { faSearch } from '@fortawesome/free-solid-svg-icons';
 
 import getDataFromFeed from './load-data/index.js';
+
+/**
+ *  https://stackoverflow.com/questions/1912501/unescape-html-entities-in-javascript
+ * 
+htmlDecode("&lt;img src='myimage.jpg'&gt;"); 
+// returns "<img src='myimage.jpg'>"
+ */
+function htmlDecode(input) {
+  var e = document.createElement('textarea');
+  e.innerHTML = input;
+  // handle case of empty input
+  return e.childNodes.length === 0 ? '' : e.childNodes[0].nodeValue;
+}
+
+// const CORS_PROXY = 'https://api.allorigins.win/get?url='; /
 
 const RSS_FEEDS = require('./load-data/RSS_FEEDS.js');
 
 class App extends React.Component {
   constructor(props) {
     super(props);
-    this.state = { 
-      feedItems: []
-     };
+    this.state = {
+      feedItems: [],
+    };
   }
 
-  async componentDidMount(){
-    const self = this;
-    const rssFeedData = Promise.resolve(getDataFromFeed(RSS_FEEDS));
-    rssFeedData.then(function(data) {
-      self.setState({
-        feedItems: data, 
-        feedTitle: 'feed.title'
-      })
-    }, function(e) {
-      console.error(e); 
+  async componentDidMount() {
+    const finalRes = RSS_FEEDS.map((rss) => {
+      return fetch(rss.value)
+        .then((response) => response.text())
+        .then((str) => new window.DOMParser().parseFromString(str, 'text/xml'))
+        .then((data) => {
+          // console.log(data);
+          const icon = data.querySelector('icon').innerHTML;
+          // console.log(icon);
+          const title = data.querySelector('title').innerHTML;
+          // console.log(title);
+          const updated = data.querySelector('updated').innerHTML;
+          // console.log(updated);
+          const link = data.querySelector('link').getAttribute('href');
+          // console.log(link);
+          const publication = { title, icon, updated, link };
+          const entries = data.querySelectorAll('entry');
+          // console.log('entries', entries);
+          const parsedEntries = [...entries].map((el) => {
+            const title = el.querySelector('title').innerHTML;
+            // console.log(title);
+            const link = el.querySelector('link').getAttribute('href');
+            // console.log(link);
+            const published = el.querySelector('published').innerHTML;
+            // console.log(published);
+            const updated = el.querySelector('updated').innerHTML;
+            // console.log(updated);
+            const content = el.querySelector('content').innerHTML;
+            // console.log(content);
+            const authorName = el.querySelector('name').innerHTML;
+            // console.log(authorName);
+            return {
+              publication,
+              title,
+              link,
+              published,
+              updated,
+              content,
+              authorName,
+            };
+          });
+          console.log('parsedEntries', parsedEntries);
+          // const result = { icon, title, updated, link, entries: parsedEntries };
+          return parsedEntries;
+          // return result;
+        });
     });
+    const rs = await Promise.all(finalRes);
+    console.log('finalRes  ', rs.flat());
+    const rsFlat = rs.flat();
+    console.log('rsFat', rsFlat);
+    // console.log('finalRes.flat()', finalRes.flat());
+    this.setState({
+      feedItems: rsFlat,
+      feedTitle: 'title',
+    });
+
+    // const self = this;
+    // const rssFeedData = Promise.resolve(getDataFromFeed(RSS_FEEDS));
+    // rssFeedData.then(
+    //   function (data) {
+    //     self.setState({
+    //       feedItems: data,
+    //       feedTitle: 'feed.title',
+    //     });
+    //   },
+    //   function (e) {
+    //     console.error(e);
+    //   }
+    // );
   }
 
-  handleSearch = (e)=>{
+  handleSearch = (e) => {
     this.searchRssFeed(e.target.value);
-  }
+  };
 
-  searchRssFeed = (searchValue)=>{
+  searchRssFeed = (searchValue) => {
+    console.log('this.state.feedItems', this.state.feedItems);
     const rssFeedResults = this.state.feedItems.map((item) => {
-      if (item.title.toLowerCase().includes(searchValue.toLowerCase())
-      || item.contentSnippet.toLowerCase().includes(searchValue.toLowerCase())) {
+      if (
+        item.title.toLowerCase().includes(searchValue.toLowerCase()) ||
+        item.content.toLowerCase().includes(searchValue.toLowerCase())
+      ) {
         item.display = true;
 
         return item;
@@ -58,60 +133,67 @@ class App extends React.Component {
     });
 
     this.setState({
-      images: rssFeedResults
+      images: rssFeedResults,
     });
-  }
+  };
 
   render() {
-    const feed = this.state.feedItems.map((item,index)=>{
+    const feed = this.state.feedItems.map((item, index) => {
       if (item.display) {
-      return (
-        <ListGroup.Item key={item.link+index}>
-          <Row>
-            <Col xl={9} lg={9} md={9} sm={9}  xs={12}>
-              <details>
-                <summary title={item.pubDate}>
-                  <a href= {item.link} target="_blank" rel="noopener noreferrer">{item.title}</a>
-                </summary>
-                <div dangerouslySetInnerHTML={{__html: item.content}}/>
-                <small className={'text-secondary'}>{item.author}</small>
-              </details>
-            </Col>
-            <Col xl={3} lg={3} md={3} sm={3}  xs={12}>
-              <Badge className={'noselect'} variant="dark">{item.brand}</Badge>
-            </Col>
-          </Row>
-        </ListGroup.Item>
-       )
-      }
-      else {
+        return (
+          <ListGroup.Item key={item.link + index}>
+            <Row>
+              <Col xl={9} lg={9} md={9} sm={9} xs={12}>
+                <details>
+                  <summary title={item.pubDate}>
+                    <img
+                      style={{ width: '20px', height: '20px' }}
+                      src={item.publication.icon}
+                    ></img>
+                    <a href={item.link} target="_blank" rel="noopener noreferrer">
+                      {item.title}
+                    </a>
+                  </summary>
+                  <div dangerouslySetInnerHTML={{ __html: htmlDecode(item.content) }} />
+                  <small className={'text-secondary'}>{item.author}</small>
+                </details>
+              </Col>
+              <Col xl={3} lg={3} md={3} sm={3} xs={12}>
+                <Badge className={'noselect'} variant="dark">
+                  {item.brand}
+                </Badge>
+              </Col>
+            </Row>
+          </ListGroup.Item>
+        );
+      } else {
         return null;
       }
-    })
+    });
     return (
       <Container>
         <Card>
-        <Card.Header>
-          <InputGroup className="mb-3">
-            <InputGroup.Prepend>
-              <InputGroup.Text id="basic-addon1">
-                <FontAwesomeIcon icon={ faSearch } />
-              </InputGroup.Text>
-            </InputGroup.Prepend>
-            <FormControl
-              placeholder="Search news feeds.."
-              aria-label="search"
-              aria-describedby="basic-addon1"
-              onChange={this.handleSearch}
-            />
-          </InputGroup>
-        </Card.Header>
-        <ListGroup variant="flush" style={{ height: '90vh',overflow: 'auto'}}>
-        {feed}
-        </ListGroup>
+          <Card.Header>
+            <InputGroup className="mb-3">
+              <InputGroup.Prepend>
+                <InputGroup.Text id="basic-addon1">
+                  <FontAwesomeIcon icon={faSearch} />
+                </InputGroup.Text>
+              </InputGroup.Prepend>
+              <FormControl
+                placeholder="Search news feeds.."
+                aria-label="search"
+                aria-describedby="basic-addon1"
+                onChange={this.handleSearch}
+              />
+            </InputGroup>
+          </Card.Header>
+          <ListGroup variant="flush" style={{ height: '90vh', overflow: 'auto' }}>
+            {feed}
+          </ListGroup>
         </Card>
       </Container>
-      ); 
+    );
   }
 }
 
--- src/load-data/index.js
+++ src/load-data/index.js
@@ -2,14 +2,17 @@
  * async array modified From https://codeburst.io/javascript-async-await-with-foreach-b6ba62bbf404
  */
 const Parser = require('rss-parser');
-const CORS_PROXY = "https://cors-anywhere.herokuapp.com/";
+// const CORS_PROXY = 'https://cors-anywhere.herokuapp.com/';
+const CORS_PROXY = 'https://api.allorigins.win/get?url='; //+ encodeURIComponent(url);
 let parser = new Parser();
 
 const fetchDataFromFeed = async (rssFeedEndPoint) => {
-  return await parser.parseURL(CORS_PROXY + rssFeedEndPoint);
+  const feed = await parser.parseURL(CORS_PROXY + encodeURIComponent(rssFeedEndPoint));
+  console.log(feed);
+  return feed;
+
   // return await parser.parseURL(rssFeedEndPoint);
 };
-
 
 async function asyncForEach(array, callback) {
   for (let index = 0; index < array.length; index++) {
@@ -17,16 +20,19 @@ async function asyncForEach(array, callback) {
   }
 }
 
-export default async function getDataFromFeed (RSS_FEEDS) {
+export default async function getDataFromFeed(RSS_FEEDS) {
   let result = [];
-   await asyncForEach(RSS_FEEDS, async (rssFeed) => {
+  await asyncForEach(RSS_FEEDS, async (rssFeed) => {
     try {
       let feed = await fetchDataFromFeed(rssFeed.value);
-      const newFeedItems = await feed.items.map((item)=>{ item.display = true; item.brand = feed.title; return item;});
+      const newFeedItems = await feed.items.map((item) => {
+        item.display = true;
+        item.brand = feed.title;
+        return item;
+      });
       result = result.concat(newFeedItems);
-    }
-    catch (e){
-      console.log(`error for ${rssFeed.value}::`, e)
+    } catch (e) {
+      console.log(`error for ${rssFeed.value}::`, e);
     }
   });
   // console.log('Done', result);
@@ -34,4 +40,3 @@ export default async function getDataFromFeed (RSS_FEEDS) {
 }
 
 // module.exports = getDataFromFeed;
-
